f34ce5f72e9c90426fe2f0e01b0128d1ff482d23
gangadhar <gangadharlandge8@gmail.com>
2021-12-19 08:59:50 -0800

    adding changed files

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -1,13 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 
-var arrow="<"
+const userDetails = {
+  customerName: "xyz",
+  email: "abc@gmail.com",
+  phone: "+263738492",
+  appointmentOn:new Date(238936),
+  status: "In progress",
+  door: "Mark",
+  companyName: "Alan Ford",
+  num: '00005152'
+}
+
+
 function App() {
   return (
     <div className="site-container">
-    {/* Start Your code here */}
+      <header className="header">
+        <label>{userDetails.companyName}</label>
+        <h5>{userDetails.num}</h5>
+      </header>
+      <div className="customer-info">
+        <table>
+          
+          <tr>
+            <td><b>Appointment on:</b></td>
+            <td>{userDetails.appointmentOn.toLocaleDateString}</td>
+          </tr>
+          <tr>
+            <td><b>Email:</b></td>
+            <td>{userDetails.email}</td>
+          </tr>
+          <tr>
+            <td><b>Phone:</b></td>
+            <td>{userDetails.phone}</td>
+          </tr>
+        
+        </table>
+        
+      </div>
+      <div className="order-info">
+      <table>
+          <tr>
+            <td><b>Status</b></td>
+            <td><b>Door</b></td>
+            <td><b>Time</b></td>
+          </tr>
+          <tr>
+            <td>{userDetails.status}</td>
+            <td> {userDetails.door}</td>
+            <td>{userDetails.date}</td>
+          </tr>
+        </table>
+      </div>
+      <div className="product-list">
+        <input className="product-containt" type="checkbox" />
+        <div className="product-containt">
+        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="customerImg" />
+        </div>
+        <div className="product-containt">
+        <h5>{userDetails.customerName}</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur explicabo cum sed cumque itaque nobis? Dolorem corporis deserunt perspiciatis, possimus porro, optio impedit dolorum aperiam facere minima est fugiat omnis!</p>
+        </div>     
+      </div>
     </div>
   );
 }
